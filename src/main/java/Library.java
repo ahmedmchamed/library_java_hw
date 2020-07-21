565c6ba94346c316e2536fd596e397d575b15223
Ahmed Hamed <tunashark@gmail.com>
2020-07-21 17:21:46 +0100

    added number of books in stock

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,11 +3,15 @@ import java.util.ArrayList;
 public class Library {
 
     private ArrayList<Book> booksStock;
+    private int capacity;
 
-    public Library() {
+    public Library(int capacity) {
         this.booksStock = new ArrayList<Book>();
+        this.capacity = capacity;
     }
 
-    
+    public int getNumberOfBooks() {
+        return this.booksStock.size();
+    }
 
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library lovelyLibrary;
+    private Book funnyBook;
+    private Book suspenseBook;
+    private Book thrillerBook;
+
+    @Before
+    public void before() {
+        lovelyLibrary = new Library(100);
+        funnyBook = new Book("Teehee", "Author McGhee", "Fiction");
+        suspenseBook = new Book("Woah", "Middlename Danger", "Nonfiction");
+        thrillerBook = new Book("Into The Woods", "McMuffin", "Thriller");
+    }
+
+    @Test
+    public void stockIsEmpty() {
+        assertEquals(0, lovelyLibrary.getNumberOfBooks());
+    }
+
 }
