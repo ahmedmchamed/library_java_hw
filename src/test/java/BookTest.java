11532f93836f7990051d2f65029e46a01bd67b13
Ahmed Hamed <tunashark@gmail.com>
2020-07-21 17:40:59 +0100

    added Book tests

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,3 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    private Book book;
+
+    @Before
+    public void before() {
+        book = new Book("Something Great", "Ralph", "Awesomeness");
+    }
+
+    @Test
+    public void canGetTitle() {
+        assertEquals("Something Great", book.getTitle());
+    }
+
+    @Test
+    public void canGetAuthor() {
+        assertEquals("Ralph", book.getAuthor());
+    }
+
+    @Test
+    public void canGetGenre() {
+        assertEquals("Awesomeness", book.getGenre());
+    }
 
 }
