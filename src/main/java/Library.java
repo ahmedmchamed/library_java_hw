c6fb4dabfe616fdd69fe8643dcd63109d59f52c5
Ahmed Hamed <tunashark@gmail.com>
2020-07-21 17:04:41 +0100

    added Library constructor

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> booksStock;
+
+    public Library() {
+        this.booksStock = new ArrayList<Book>();
+    }
+
+    
+
 }
